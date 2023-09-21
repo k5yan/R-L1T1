0e0c4918738973ac8952db66ac6041a1c3ac7db7
ivan <koncept.th@gmail.com>
2023-09-21 14:41:25 +0500

    to JS

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 export function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{new Date().getFullYear()}</p>
-			</header>
-		</div>
+	return React.createElement(
+		`div`,
+		{
+			className: 'App',
+		},
+		React.createElement(
+			`header`,
+			{
+				className: 'App-header',
+			},
+			React.createElement(`img`, {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				{},
+				'Edit ',
+				React.createElement('code', {}, 'src/App.js'),
+				' and save to reload',
+			),
+			React.createElement(
+				`a`,
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				`Learn React`,
+			),
+			React.createElement(`p`, {}, `${new Date().getFullYear()}`),
+		),
 	);
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<p>{new Date().getFullYear()}</p>
+	// 		</header>
+	// 	</div>
+	// );
 }
